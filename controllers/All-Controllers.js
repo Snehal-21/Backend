609c066af734701980613e7102c71d02ae806c30
Snehal Navale <navalesnehal21@gmail.com>
2023-05-18 10:18:37 +0530

    controllers

---
+++ controllers/All-Controllers.js
@@ -0,0 +1,12 @@
+export const home=(req,res)=>{
+    res.send("home function");
+}
+
+
+export const snehal=(req,res)=>{
+    res.send("sending message from snehal function");
+}
+
+export const navale=(req,res)=>{
+    res.send("sending message from navale function");
+}
--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-console.log("hello world");
-console.log("bye bye");
+import express from "express";
+import{snehal,navale,home} from './controllers/All-Controllers.js';
+const app=express();
+app.get("/",home);
+app.get("/snehal",snehal);
+app.get("/navale",navale);
+
+app.listen(8000);
