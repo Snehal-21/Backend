218536e3c2be5cc569e0cff480c449b05ef67a7a
Snehal Navale <navalesnehal21@gmail.com>
2023-05-24 11:27:23 +0530

    mongoose updates

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ import express from "express";
 import morgan from "morgan";
 import{snehal,navale,home, error} from './controllers/All-Controllers.js';
 import router from './routes/userRoutes.js';
+import mongoose from "mongoose";
 const app=express();
+
 
 app.use(morgan('dev'));//middleware
 app.use(express.json());//used to parse the data
 app.use('/api/v1',router);
 
-// app.get("/",home);
-// app.get("/snehal",snehal);
-// app.get("/navale",navale);
-// app.get("/error",error);//pass two parameters first is path second is function
+mongoose.connect('mongodb+srv://Snehal:Snehal1234@mern-todo.va7rcii.mongodb.net/')
+.then(() => console.log("DB connected successfully"))
+.catch((err)=> console.log("DB error =>",err));
 
 app.listen(8000,()=>console.log("working on port 8000"));//port
