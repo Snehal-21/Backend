3bce3956d7d93657af84aa82a1d3b372168e21bc
Snehal Navale <navalesnehal21@gmail.com>
2023-06-02 12:17:52 +0530

    otp update

---
+++ controllers/otpControllers.js
@@ -0,0 +1,45 @@
+import {v4 as uuidv4} from 'uuid';
+import Users from '../modals/Users.js';
+export const otpRegistration=async(req,res)=>{
+    try{
+        const {number,email}=req.body;
+        if (!number) return res.send("Number is required!");
+        if(!email) return res.send("Email is required!");
+        var code = uuidv4();
+        // res.send(code);
+        const isNumberPresent=await Users.find({number}).exec();
+        if(!isNumberPresent.length) return res.send("Number already used!");
+
+        const isEmailPresent= await Users.find({email}).exec();
+        if(!isEmailPresent.length) return res.send("Email already talen!")
+
+        const user=new Users({
+            email,
+            number,
+            otp:code
+        })
+        await user.save();
+        res.send("Check your mobile and gmail foe Otp")
+    }catch(error){
+        res.send(error);
+    }
+}
+
+export const otpCheckForRegister=async(req,res)=>{
+    try{
+        const {number,email,otp}=req.body;
+        if (!number) return res.send("Number is required");
+        if (!email) return res.send("email is required");
+        if (!otp) return res.send("otp is required");
+
+        const user=await Users.find({email}).exec();
+        if(!user.length) return res.send("user not found");
+        if(user[0].otp==otp){
+            return res.send("Registration done");
+        }
+       return res.send("wrong otp")
+
+    }catch(error){
+        return res.send(error)
+    }
+}
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import { addProduct, getAllProducts } from "../controllers/productsController.js
 import { login, register, updateuser } from "../controllers/userControllers.js";
 import express from "express";
 import { checkEmail, checkName } from "../middlewares/authMiddleware.js";
+import { otpCheckForRegister, otpRegistration } from "../controllers/otpControllers.js";
 
 const router=express.Router();
 
@@ -10,4 +11,6 @@ router .post('/register',checkName,register)
 router.post('/add-Product',addProduct)
 router.get('/get-all-products',getAllProducts)
 router.post('/update/',checkEmail,updateuser)
+router.post('/otp-register',otpRegistration)
+router.post('/otp-check-register',otpCheckForRegister)
 export default router;
